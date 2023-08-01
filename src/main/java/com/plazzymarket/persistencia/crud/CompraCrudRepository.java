436476da26110d076ba8aa78a3fc622c144e4d65
Arroyo <arroyolokura8@gmail.com>
2023-08-01 10:22:50 -0500

    Configuracion para desplegar

--- src/main/java/com/plazzymarket/persistencia/crud/CompraCrudRepository.java
+++ src/main/java/com/plazzymarket/persistencia/crud/CompraCrudRepository.java
@@ -2,10 +2,13 @@ package com.plazzymarket.persistencia.crud;
 
 import com.plazzymarket.persistencia.entity.Compra;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
-
+@Service
+@Component
 public interface CompraCrudRepository extends CrudRepository<Compra, Integer> {
 
     Optional<List<Compra>> findByIdCliente(String idCliente);
